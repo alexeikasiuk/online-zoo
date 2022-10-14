f6066ba086c1f5165e42fb328ba69968e133d754
alexeikasiuk <aliakseikasiuk@gmail.com>
2022-10-14 18:23:44 +0300

    fix: change the behavior of the burger menu on the main page

--- pages/main/script.js
+++ pages/main/script.js
@@ -5,21 +5,21 @@ const mobileSize = 450;
 let screenType;
 
 // burger menu
-const iconMenu = document.querySelector('.menu_icon');
+const burgerMenu = document.querySelector('.burger_icon');
 const menuBody = document.querySelector('.menu_body');
 const headerLogo = document.querySelector('header').querySelector('.logo');
 
 // open/close burger menu by click on burger menu icon
-iconMenu.addEventListener('click', function (e) {
-  iconMenu.classList.toggle('_active');
+burgerMenu.addEventListener('click', function (e) {
+  burgerMenu.classList.toggle('_active');
   document.body.classList.toggle('_lock');
   menuBody.classList.toggle('_active');
   headerLogo.classList.toggle('burger_logo');
 });
 // close opened burger menu by click on free area burger menu
 menuBody.addEventListener('click', (e) => {
-  if (menuBody.classList.contains('_active')) {
-    iconMenu.classList.toggle('_active');
+  if (menuBody.classList.contains('_active') && e.target === menuBody) {
+    burgerMenu.classList.toggle('_active');
     document.body.classList.toggle('_lock');
     menuBody.classList.toggle('_active');
     headerLogo.classList.toggle('burger_logo');
