6e7e70ec7e2de6db9a390583e2f85bcf971963be
Aliaksei Kasiuk <aliakseikasiuk@gmail.com>
2022-10-29 14:31:22 +0300

    feat: add header & footer  section & the burger menu header section for tablet/mobile screen for the zoo page

---
+++ pages/zoo/script.js
@@ -0,0 +1,32 @@
+'user strict';
+// burger menu
+const burgerMenu = document.querySelector('.burger_icon');
+const menuBody = document.querySelector('.menu_body');
+const headerLogo = document.querySelector('header').querySelector('.logo');
+
+// open/close burger menu by click on burger menu icon
+burgerMenu.addEventListener('click', function (e) {
+  burgerMenu.classList.toggle('_active');
+  document.body.classList.toggle('_lock');
+  menuBody.classList.toggle('_active');
+  headerLogo.classList.toggle('burger_logo');
+});
+// close opened burger menu by click on free area burger menu
+menuBody.addEventListener('click', (e) => {
+  if (menuBody.classList.contains('_active') && e.target === menuBody) {
+    burgerMenu.classList.toggle('_active');
+    document.body.classList.toggle('_lock');
+    menuBody.classList.toggle('_active');
+    headerLogo.classList.toggle('burger_logo');
+  }
+});
+
+// subscribe form
+document.onsubmit = (e) => {
+  e.preventDefault();
+  alert(
+    `Success subscribe!!!\nemail: ${
+      document.querySelector('[type="email"]').value
+    }`
+  );
+};
